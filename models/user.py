efa1779c27e0a89b35b0b5ec4deab53d9431bab3
Felipe Murilo Alves Torres <felipemurilooutlok9@gmail.com>
2026-01-07 22:05:28 -0300

    pré-cadastro por cpf

--- models/user.py
+++ models/user.py
@@ -7,4 +7,11 @@ class User(db.Model, UserMixin):
     cpf = db.Column(db.String(11), unique=True, nullable=False)
     senha = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), nullable=False)
-    ativo = db.Column(db.Boolean, default=True)  # e.g., 'admin' or ''
+    ativo = db.Column(db.Boolean, default=True)  
+
+class PreAuthorizedUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    nome = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    usado = db.Column(db.Boolean, default=False)    
